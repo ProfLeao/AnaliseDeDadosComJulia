42926b35e7dfce5bbe3c880b709c1129da27ece2
ProfLeao <prof.reginaldo.leao@gmail.com>
2022-11-02 21:37:44 -0300

    Inicio da estrutura de diretórios e inserção do primeiro notebook para arrays.

---
+++ Arr_ntb1.jl
@@ -0,0 +1,33 @@
+### A Pluto.jl notebook ###
+# v0.19.14
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 4385af42-5b0f-11ed-0874-ebf4d0440e2f
+md"""
+# Introdução às _**Arrays**_ de Julia
+## Autor: Prof. Dr. R. G. Leão Jr. 
+### prof.reginaldo.leao at gmail . com . br
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.8.2"
+manifest_format = "2.0"
+project_hash = "da39a3ee5e6b4b0d3255bfef95601890afd80709"
+
+[deps]
+"""
+
+# ╔═╡ Cell order:
+# ╠═4385af42-5b0f-11ed-0874-ebf4d0440e2f
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
